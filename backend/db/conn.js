81d059762f509c08397c1e226e33679f9c5fcf06
Bryan Prinz <146386080+Bryanps7@users.noreply.github.com>
2025-10-06 11:52:59 -0300

    v2025.10.06

---
+++ backend/db/conn.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('project-lego', 'root', 'senai', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+sequelize.authenticate()
+.then(()=>{
+    console.log('> conexão com o banco de dados estabelecida com sucesso.');
+})
+.catch((err)=>{
+    console.error('> falha ao conectar ao banco de dados: ', err);
+});
+
+module.exports = sequelize;
